0fb4e9807c3768ce73e953fe1b458c50e59fc224
BalajiSai2003 <balajisai2003@outlook.com>
2025-02-13 12:28:14 +0530

    repeat.for, click delegate

--- Todo/src/app.js
+++ Todo/src/app.js
@@ -2,7 +2,21 @@ import { Todo } from "./todo";
 
 export class App{
   constructor(){
-    this.todo = new Todo("Clean my room");
+    this.todoList = [];
+    this.todoList.push(new Todo("Clean my room"));
+    this.todoList.push(new Todo("Drink Water"));
+    this.todoList.push(new Todo("Eat"));
+    this.todoList.push(new Todo("Dance"));
+    this.todoList.push(new Todo("Sleep"));
+    this.newItem = "";
+  }
 
+  addTodo(){
+    this.todoList.push(new Todo(this.newItem))
+    this.newItem = "";
+  }
+
+  removeTodo(todo){
+    this.todoList.splice(this.todoList.indexOf(todo),1)
   }
 }
