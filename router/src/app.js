4ac7be578c082b51fdd0b5db6a264506386527a1
BalajiSai2003 <balajisai2003@outlook.com>
2025-02-13 12:55:54 +0530

    Routing Setup

--- router/src/app.js
+++ router/src/app.js
@@ -1,3 +1,13 @@
+import {PLATFORM} from 'aurelia-framework'
+
 export class App {
-  message = 'Hello World!';
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Router Test';
+    config.map([
+      { route: ['','Home'], name: 'home', moduleId : PLATFORM.moduleName('index'), title:'Home' },
+      { route: 'about', name: 'about', moduleId : PLATFORM.moduleName('about'), title:'Home' }
+
+    ]);
+  }
 }
